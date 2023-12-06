3a74ab7c3b8f2a72599e91f787c14f5d81f4612e
ragav7332 <ragav7332@gmail.com>
2023-12-06 18:36:20 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/sb-admin-2.css";
+import DashBoard from "./DashBoard";
+import "bootstrap/dist/js/bootstrap";
+
+import Users from "./Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./Products";
+import CreateUser from "./CreateUser";
+import CreateProduct from "./CreateProduct";
+import Portal from "./Portal";
+import Login from "./Login";
+import UserView from "./UserView";
+import EditUser from "./EditUser";
+import EditProduct from "./EditProduct";
+import ProductView from "./ProductView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/portal" element={<Portal />}>
+          <Route path="dashboard" element={<DashBoard />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="users/view/:userid" element={<UserView />}></Route>
+          <Route path="users/edit/:userid" element={<EditUser />}></Route>
+          <Route path="create-user" element={<CreateUser />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="products/view/:userid" element={<ProductView />}></Route>
+          <Route path="products/edit/:userid" element={<EditProduct />}></Route>
+          <Route path="create-product" element={<CreateProduct />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
